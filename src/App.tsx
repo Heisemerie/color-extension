e52d404ce6458eb7413f36d28a1314fcf9fb979b
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-09-28 00:59:36 +0100

    implement click to change backgroud color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const onclick = async () => {
+    // call chrome tab api for active tab
+    let [tab] = await chrome.tabs.query({ active: true });
+
+    // call chrome script api
+    if (tab.id !== undefined) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          document.body.style.backgroundColor = "red";
+        },
+      });
+    } else {
+      console.error("Active tab id is undefined.");
+    }
+  };
 
   return (
     <>
@@ -16,11 +30,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Color Extension</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => onclick()}>Click Me!</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +41,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
